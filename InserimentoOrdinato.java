dcdf0bc7185282871bb9d249a7e6ab202f22291d
Nikey <160123565+NikiValle@users.noreply.github.com>
2024-02-23 13:57:36 +0100

    Add files via upload

--- InserimentoOrdinato.java
+++ InserimentoOrdinato.java
@@ -5,15 +5,23 @@ public class InserimentoOrdinato{
         String [] stringa = new String [100];
         String attuale;
         boolean fine =false;
+        int lung =0;
         do{
             System.out.println("Inserisci una stringa");
             attuale=in.next();
             if(attuale.equalsIgnoreCase("fine")){
                 fine=true;
             }
-            else{
-                
+            else {
+                for (int j = 0; j < lung; j++) {
+                    for (int i = 0; i < attuale.length(); i++) {
+                        if (attuale.charAt(i) > stringa[j].charAt(i)){
+                            
+                        }
+                    }
+                }
             }
+            lung=lung+1;
         }while(fine=false);
     }
 }
